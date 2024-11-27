903aa7d5da5bf674b7ef1ed439588376660f89fb
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2024-11-27 17:29:17 +0100

    Addi the solution to task 2.

--- src/main/java/pl/vistula/Main.java
+++ src/main/java/pl/vistula/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         out.println("Result from task 1: " + generatorStrChar(5));
         out.println("Result from task 1: " + generatorStrChar(8));
+        out.println("The sum of the numbers in the array is: "  + calcSumArray(new int[]{8,1,2,3,4}));
     }
 
     public static StringBuilder generatorStrChar(int n) {
@@ -22,5 +23,13 @@ public class Main {
         }
 
         return strChar;
+    }
+
+    private static int calcSumArray(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+        return result;
     }
 }
