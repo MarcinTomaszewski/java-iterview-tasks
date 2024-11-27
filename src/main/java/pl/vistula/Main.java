4b72c317d2c57e09ce48594c08a563cc59eec8e1
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2024-11-27 17:26:15 +0100

    Addi the solution to task 1.

--- src/main/java/pl/vistula/Main.java
+++ src/main/java/pl/vistula/Main.java
@@ -1,7 +1,26 @@
 package pl.vistula;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        out.println("Result from task 1: " + generatorStrChar(5));
+        out.println("Result from task 1: " + generatorStrChar(8));
+    }
+
+    public static StringBuilder generatorStrChar(int n) {
+        StringBuilder strChar = new StringBuilder();
+        char firstChar = '+';
+        char secondChar = '-';
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                strChar.append(firstChar);
+            } else {
+                strChar.append(secondChar);
+            }
+        }
+
+        return strChar;
     }
 }
